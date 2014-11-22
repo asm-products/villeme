7d4f8ee0b6b93ce017ffd567ca39240191c0589e
jonatassalgado <jonatas.salgado@acad.pucrs.br>
2014-11-22 18:12:11 -0200

    Module "Geocoderize" created in /lib and extend on "Place" to geocoder objects place from :address attribute

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,6 @@
 class Place < ActiveRecord::Base
 
-
-	# validações
-	
+	extend Geocoderize
 
 	# associações
 	has_many :events, dependent: :destroy
@@ -12,43 +10,12 @@ class Place < ActiveRecord::Base
 
 
 	# Geocoder
-	geocoded_by :address do |place, geocoder_results|
-		geocoder  = geocoder_results.first
-
-		if geocoder
-			place.latitude = geocoder.latitude
-			place.longitude = geocoder.longitude
-			place.street_number = geocoder.street_number
-			place.postal_code = geocoder.postal_code
-			place.neighborhood = geocoder.address_components_of_type(:neighborhood).first["long_name"]
-			place.city = geocoder.city
-			place.full_address = geocoder.address
-			place.state = geocoder.state
-			place.state_code = geocoder.state_code
-			place.country_code = geocoder.country_code
-			place.country = geocoder.country
-			place.formatted_address = "#{geocoder.address_components_of_type(:route).first["short_name"]}, #{geocoder.street_number} - #{geocoder.address_components_of_type(:neighborhood).first["long_name"]}"
-		end
-	end
-
-	after_validation :geocode
+	geocoded_by_address
 
 
 
-	def copy_attributes_to(event)
-    event.address = self.address	
-    event.number = self.number
-    event.neighborhood_name = self.neighborhood_name
-    event.city_name = self.city_name
-    event.postal_code = self.postal_code
-    event.state = self.state
-    event.state_code = self.state_code
-    event.country = self.country
-    event.country_code = self.country_code
-    event.full_address = self.full_address
-    event.latitude = self.latitude
-    event.longitude = self.longitude
-	end
+
+
 
 
 end
---
+++ lib/geocoderize.rb
@@ -0,0 +1,45 @@
+module Geocoderize
+
+
+  def geocoded_by_address
+    geocoded_by :address do |object, geocoder_results|
+      geocoder = geocoder_results.first
+
+      if geocoder
+        object.latitude = geocoder.latitude
+        object.longitude = geocoder.longitude
+        object.street_number = geocoder.street_number
+        object.postal_code = geocoder.postal_code
+        object.neighborhood = geocoder.address_components_of_type(:neighborhood).first["long_name"]
+        object.city = geocoder.city
+        object.full_address = geocoder.address
+        object.state = geocoder.state
+        object.state_code = geocoder.state_code
+        object.country_code = geocoder.country_code
+        object.country = geocoder.country
+        object.formatted_address = "#{geocoder.address_components_of_type(:route).first["short_name"]}, #{geocoder.street_number} - #{geocoder.address_components_of_type(:neighborhood).first["long_name"]}"
+      end
+    end
+
+    after_validation :geocode
+  end
+
+
+
+  def copy_attributes_to(object)
+    object.address = self.address
+    object.number = self.number
+    object.neighborhood_name = self.neighborhood_name
+    object.city_name = self.city_name
+    object.postal_code = self.postal_code
+    object.state = self.state
+    object.state_code = self.state_code
+    object.country = self.country
+    object.country_code = self.country_code
+    object.full_address = self.full_address
+    object.latitude = self.latitude
+    object.longitude = self.longitude
+  end
+
+
+end
