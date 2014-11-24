ac0859aa96c1b2f03b978ce11418938b0ddb09f1
jonatassalgado <jonatas.salgado@acad.pucrs.br>
2014-11-23 22:12:37 -0200

    Slug generator added on "City" model

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,15 @@
 class City < ActiveRecord::Base
 
+  extend FriendlyId
+  friendly_id :slug_candidates, use: :slugged
 
+  # Try building a slug based on the following fields in
+  # increasing order of specificity.
+  def slug_candidates
+    [
+        :name,
+        [:name, :id]
+    ]
+  end
 
 end
