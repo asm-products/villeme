ed4a3f0d223beca39e539051492e8b1a843775c3
jonatassalgado <jonatas.salgado@acad.pucrs.br>
2014-11-23 22:13:38 -0200

    Slug generator added on "Neighborhood" model

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,8 +1,15 @@
 class Neighborhood < ActiveRecord::Base
 
   extend FriendlyId
-  friendly_id :name, use: :slugged	
+  friendly_id :name, use: :slug_candidates
 
-
+  # Try building a slug based on the following fields in
+  # increasing order of specificity.
+  def slug_candidates
+    [
+        :name,
+        [:name, :id]
+    ]
+  end
 
 end
