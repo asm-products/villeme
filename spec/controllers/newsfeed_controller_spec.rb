af64ef052f53531b423f742a9fc65f679c06e97f
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2014-12-02 00:05:47 -0200

    Adjusts in RSpec

--- spec/controllers/newsfeed_controller_spec.rb
+++ spec/controllers/newsfeed_controller_spec.rb
@@ -3,14 +3,21 @@ require 'rails_helper'
 describe NewsfeedController do
 
   describe '#index' do
-    it 'should be load the page with success' do
-      user = build(:user)
-      allow(controller).to receive(:current_user).and_return(user)
 
-      get :index, locale: :en
+    context 'current_user logged in' do
+      it 'should be load the page with success' do
+        user = build(:user)
+        allow(controller).to receive(:current_user).and_return(user)
 
-      expect(response.status).to eq(200)
+        get :index, locale: :en
+
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'current_user NOT logged in' do
     end
 
   end
+
 end
