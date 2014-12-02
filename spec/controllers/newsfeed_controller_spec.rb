5ea71a510dc07a0c6c7a11900a1d176480a84006
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2014-12-01 23:58:54 -0200

    Test create to successful request in NewsfeedControlle

--- spec/controllers/newsfeed_controller_spec.rb
+++ spec/controllers/newsfeed_controller_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-describe WelcomeController, type: :controller do
+describe NewsfeedController do
 
   describe '#index' do
-    it "should load with success" do
-
-      user = User.new
+    it 'should be load the page with success' do
+      user = build(:user)
       allow(controller).to receive(:current_user).and_return(user)
 
       get :index, locale: :en
+
       expect(response.status).to eq(200)
     end
-  end
 
+  end
 end
