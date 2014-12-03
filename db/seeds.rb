bddee3a6072fdf94d801d11ff7fcabcf1b0b2b7e
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2014-12-03 17:27:56 -0200

    Adjust in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -188,7 +188,7 @@ weeks = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Su
 n = 1
 
 weeks.each do |name|
-  week = Category.create(
+  week = Week.create(
       name: name,
       slug: name.downcase,
       organizer_id: n
