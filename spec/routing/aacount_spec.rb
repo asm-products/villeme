f5867aad68a707220853594d7fe5bc06645253ef
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2014-12-04 23:51:36 -0200

    RSpec test in account#edit route

--- spec/routing/aacount_spec.rb
+++ spec/routing/aacount_spec.rb
@@ -1,16 +1,21 @@
 require 'rails_helper'
 
-describe '/:id/account' do
+describe '/account' do
 
-  it 'should load user account' do
-    user = double("User")
-    allow(user).to receive(:id).and_return(1)
+  describe '/:id/account' do
 
-    expect(get: "/#{user.id}/account").to route_to(
-                                              controller: 'accounts',
-                                              action: 'edit',
-                                              id: "1"
-                                          )
+    it 'should load user account' do
+      user = double("User")
+      allow(user).to receive(:id).and_return(1)
+
+      expect(get: "/#{user.id}/account").to route_to(
+                                                controller: 'accounts',
+                                                action: 'edit',
+                                                id: "1"
+                                            )
+    end
+
   end
 
 end
+
