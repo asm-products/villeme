bd30a07d71e427db13bc04ceff9e18e307f37ca5
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2014-12-08 18:04:50 -0200

    Refactor on RSpec tests from User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 describe User, type: :model do
 
+  let(:user){ create(:user) }
+
   describe '#neighborhood' do
     it 'should return neighborhood for user' do
-      user = create(:user, neighborhood_name: 'Partenon')
-      neighborhood = create(:neighborhood, name: 'Partenon')
+      neighborhood = create(:neighborhood, name: 'Albany County')
 
       expect(user.neighborhood).to eq(neighborhood)
     end
@@ -13,7 +14,6 @@ describe User, type: :model do
 
   describe '#city' do
     it 'should return city for user' do
-      user = create(:user)
       city = create(:city)
 
       expect(user.city).to eq(city)
@@ -22,7 +22,6 @@ describe User, type: :model do
 
   describe '#state' do
     it 'should return state for user' do
-      user = create(:user)
       state = create(:state)
 
       expect(user.state).to eq(state)
@@ -31,7 +30,6 @@ describe User, type: :model do
 
   describe '#country' do
     it 'should return country for user' do
-      user = create(:user, country_name: 'United States')
       country = create(:country, name: 'United States')
 
       expect(user.country).to eq(country)
@@ -40,7 +38,6 @@ describe User, type: :model do
 
   describe '#events' do
     it 'should return events belongs to user' do
-      user = create(:user)
       events = [create(:event, name: 'Campus Party', user_id: 1),
                 create(:event, name: 'Hackaton', user_id: 1)]
                 create(:event, name:'Octoberfest')
