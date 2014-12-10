3a5f19a0ad20f212541d28832e3b5c829e1c3f85
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2014-12-09 23:26:39 -0200

    GeocoderMethods included to City model

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,6 +3,8 @@ class City < ActiveRecord::Base
   extend FriendlyId
   friendly_id :slug_candidates, use: :slugged
 
+  include GeocodedActions
+
   # Try building a slug based on the following fields in
   # increasing order of specificity.
   def slug_candidates
