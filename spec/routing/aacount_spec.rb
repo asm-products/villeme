79eeb30ea03c0601f0c53f50ebca052748c60e47
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2014-12-20 17:05:45 -0200

    Fix: Adjusts test for account route

--- spec/routing/aacount_spec.rb
+++ spec/routing/aacount_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 describe '/account' do
 
-  describe '/:id/account' do
+  describe 'user/:id/account' do
 
     it 'should load user account' do
       user = double("User")
       allow(user).to receive(:id).and_return(1)
 
-      expect(get: "/#{user.id}/account").to route_to(
+      expect(get: "user/#{user.id}/account").to route_to(
                                                 controller: 'accounts',
                                                 action: 'edit',
                                                 id: "1"
