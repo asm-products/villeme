dbb900c84816d3bfa53b0ce42c33439af770dee3
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2015-01-14 20:15:50 -0200

    Adjusts in tests

--- spec/usecases/get_persona_name_spec.rb
+++ spec/usecases/get_persona_name_spec.rb
@@ -5,12 +5,16 @@ describe 'UseCases::GetPersona' do
 
   describe '.find_by_id' do
     it 'should return Entrepreuner' do
+      I18n.locale = :en
+
       result = Villeme::UseCases::GetPersonaName.find_by_id(1)
 
       expect(result).to eq('Entrepreuner')
     end
 
     it 'should return alert of do not exist persona id' do
+      I18n.locale = :en
+
       result = Villeme::UseCases::GetPersonaName.find_by_id(10)
 
       expect(result).to eq('Without persona')
