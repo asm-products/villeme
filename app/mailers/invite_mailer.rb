9e541858d582b28f1eac0e97ee898d65ca1efb4a
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2015-01-16 22:54:31 -0200

    Invite mail refactored to accept the new request invite

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -5,15 +5,19 @@ class InviteMailer < ActionMailer::Base
   def welcome_email(invite)
     @invite = invite
     @url  = 'http://www.villeme.com/'
-    @city = @invite.city
- 
+    @city_name = @invite.city_name
+    @city_goal = 250
+    @invites_in_this_city = Invite.where(city_name: @city_name).count
+
+    set_message_about_quantity_of_invites
+
     mail(
     	to: @invite.email, 
     	from: 'jonataseduardo@villeme.com',
     	subject: 'Bem-vindo ao Villeme!',
     	track_opens: 'true'
   	)
-  end  
+  end
 
   def send_key(invite)
     @invite = invite
@@ -24,6 +28,17 @@ class InviteMailer < ActionMailer::Base
       subject: 'Seu convite para o Villeme chegou!',
       track_opens: 'true'
     )
+  end
+
+  private
+
+  def set_message_about_quantity_of_invites
+    if @city_goal <= @invites_in_this_city
+      @message = "versão Beta lançada!"
+      @city_beta_actived = true
+    else
+      @message = "<b>#{(@city_goal - @invites_in_this_city)}</b> pessoas para ativar o Beta em <b>#{@city_name}</b>".html_safe
+    end
   end
 
 end
