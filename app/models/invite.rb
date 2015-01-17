b4303f9854bddab1e549d02d2b49e64f4c8403fc
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2015-01-16 22:04:22 -0200

    Geocoder applyed in Invite model using the Use Case GeocodeInvite

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,10 +1,10 @@
 class Invite < ActiveRecord::Base
 
-  extend  GeocodedByAddress
+  require_relative '../../app/domain/usecases/geolocalization/geocode_invite'
+
+  after_validation :geocode_invite, unless: 'address.nil?'
+
   include GeocodedActions
-
-  geocoder_by_address
-
 
 	# associações
   belongs_to :user
@@ -16,6 +16,9 @@ class Invite < ActiveRecord::Base
   validates :city_sugest, length: {maximum: 140}
   validates :persona_sugest, length: {maximum: 140}
 
+  def geocode_invite
+    Villeme::UseCases::GeocodeInvite.new(self).geocoded_by_address(self.address)
+  end
 
 
 end
