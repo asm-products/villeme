140c725c5916c8360d3cb1556b134a55ec6d17b3
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2015-01-24 22:18:29 -0200

    Added tables of Ratyrate GEM

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150124201528) do
+ActiveRecord::Schema.define(version: 20150124235242) do
 
   create_table "agenda_events", force: true do |t|
     t.integer  "event_id"
