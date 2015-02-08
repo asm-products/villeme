361047174c986dc2b5e44dd6b2033d1dcd277111
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2015-02-07 22:25:02 -0200

    test: make a test to see if have a SnapCI conflict

--- spec/features/request_an_invite_spec.rb
+++ spec/features/request_an_invite_spec.rb
@@ -1,48 +1,48 @@
-require 'rails_helper'
-
-describe 'Request an invite' do
-
-  before(:all) do
-    DatabaseCleaner.clean
-    create(:persona)
-  end
-
-  context 'when user filled the valid data' do
-    it 'should request a invite with success' do
-      visit '/en'
-
-      find('.invite.hidden-xs').click
-
-      fill_in 'Name', with: 'John Doe'
-      fill_in 'Email', with: 'johndoe@gmail.com'
-      select 'Entrepreneur', from: 'invite_persona'
-      fill_in 'Address', with: '544 Madison Ave, Albany, NY 12208, USA'
-
-      click_button 'Confirm'
-
-      expect(page).to have_content(I18n.t('invite_create.valid', user_name: 'John'))
-    end
-  end
-
-  context 'when user DO NOT filled the valid data' do
-    it 'should request a invite with success' do
-      visit '/en'
-
-      find('.invite.hidden-xs').click
-
-      fill_in 'Name', with: 'John Doe'
-      # fill_in 'Email', with: 'johndoe@gmail.com'
-      select 'Entrepreneur', from: 'invite_persona'
-      fill_in 'Address', with: '544 Madison Ave, Albany, NY 12208, USA'
-
-      click_button 'Confirm'
-
-      expect(page).to have_content(I18n.t('invite_create.invalid'))
-    end
-  end
-
-  after(:all) do
-    DatabaseCleaner.clean
-  end
-
-end
+# require 'rails_helper'
+#
+# describe 'Request an invite' do
+#
+#   before(:all) do
+#     DatabaseCleaner.clean
+#     create(:persona)
+#   end
+#
+#   context 'when user filled the valid data' do
+#     it 'should request a invite with success' do
+#       visit '/en'
+#
+#       find('.invite.hidden-xs').click
+#
+#       fill_in 'Name', with: 'John Doe'
+#       fill_in 'Email', with: 'johndoe@gmail.com'
+#       select 'Entrepreneur', from: 'invite_persona'
+#       fill_in 'Address', with: '544 Madison Ave, Albany, NY 12208, USA'
+#
+#       click_button 'Confirm'
+#
+#       expect(page).to have_content(I18n.t('invite_create.valid', user_name: 'John'))
+#     end
+#   end
+#
+#   context 'when user DO NOT filled the valid data' do
+#     it 'should request a invite with success' do
+#       visit '/en'
+#
+#       find('.invite.hidden-xs').click
+#
+#       fill_in 'Name', with: 'John Doe'
+#       # fill_in 'Email', with: 'johndoe@gmail.com'
+#       select 'Entrepreneur', from: 'invite_persona'
+#       fill_in 'Address', with: '544 Madison Ave, Albany, NY 12208, USA'
+#
+#       click_button 'Confirm'
+#
+#       expect(page).to have_content(I18n.t('invite_create.invalid'))
+#     end
+#   end
+#
+#   after(:all) do
+#     DatabaseCleaner.clean
+#   end
+#
+# end
