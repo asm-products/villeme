c2b6e95b423d2b0bc5e9dc3f3aafcfc5d29d6bef
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2015-02-07 23:37:29 -0200

    Fix: refactor in CreateCityGeocoded use case

--- app/domain/usecases/geolocalization/create_city_geocoded.rb
+++ app/domain/usecases/geolocalization/create_city_geocoded.rb
@@ -8,43 +8,47 @@ module Villeme
 
 
       def create_city
-        city = City.new(address: @address)
-        geocoder = geocoder_by_address
+        @city = City.new(address: @address)
+        @geocoder = geocoder_by_address
 
-        if city_exist?(geocoder)
+        if address_or_geocoder_is_nil? || city_exist?
           false
         else
-          geocoderize_city(city, geocoder)
-          save_city?(city)
+          geocoderize_city
+          save_city?
         end
       end
 
 
       private
 
-      def city_exist?(geocoder)
-        City.find_by(name: geocoder.city)
+      def address_or_geocoder_is_nil?
+        @address.nil? || @geocoder.nil?
+      end
+
+      def city_exist?
+        City.find_by(name: @geocoder.city)
       end
 
       def geocoder_by_address
         Geocoder.search(@address).first
       end
 
-      def geocoderize_city(city, geocoder)
-        if geocoder
-          city.update_attributes(name: geocoder.city,
-          latitude: geocoder.latitude,
-          longitude: geocoder.longitude,
-          state_name: geocoder.state,
-          state_code: geocoder.state_code,
-          country_code: geocoder.country_code,
-          country_name: geocoder.country
+      def geocoderize_city
+        if @geocoder
+          @city.update_attributes(name: @geocoder.city,
+                                  latitude: @geocoder.latitude,
+                                  longitude: @geocoder.longitude,
+                                  state_name: @geocoder.state,
+                                  state_code: @geocoder.state_code,
+                                  country_code: @geocoder.country_code,
+                                  country_name: @geocoder.country
           )
         end
       end
 
-      def save_city?(object)
-        if object.save
+      def save_city?
+        if @city.save
           true
         else
           false
