a2bccf45774a98ceeb4cfd49fdc8dc4168d672b4
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2015-04-22 16:39:42 -0300

    Adjust in tests for date of week

--- spec/usecases/get_next_day_occur_human_readable_spec.rb
+++ spec/usecases/get_next_day_occur_human_readable_spec.rb
@@ -52,26 +52,32 @@ describe 'UseCases::Dates' do
     end
 
     context 'when object occur in 30 days period' do
-      it 'should return 19/Mar' do
+      it 'should return 29/Mar' do
         event = build(:event, date_start: @date_current + 10, date_finish: @date_current + 30)
-        weeks_array = [double('Week', id: 4, name: "Thursday", binary: 4, organizer_id: 4)]
+        weeks_array = [double('Week', id: 4, name: "Thursday", binary: 4, organizer_id: 4),
+                       double('Week', id: 5, name: "Friday", binary: 5, organizer_id: 5),
+                       double('Week', id: 6, name: "Saturday", binary: 6, organizer_id: 6),
+                       double('Week', id: 7, name: "Sunday", binary: 0, organizer_id: 7)]
         allow(event).to receive(:weeks).and_return(weeks_array)
 
         result = Villeme::UseCases::Dates.get_next_day_occur_human_readable(event, true)
 
-        expect(result).to eq '19/Mar'
+        expect(result).to eq '29/Mar'
       end
     end
 
     context 'when object occur in 60 days period' do
-      it 'should return 19/May' do
+      it 'should return 18/May' do
         event = build(:event, date_start: @date_current + 60, date_finish: @date_current + 65)
-        weeks_array = [double('Week', id: 4, name: "Thursday", binary: 4, organizer_id: 4)]
+        weeks_array = [double('Week', id: 1, name: "Monday", binary: 1, organizer_id: 1),
+                       double('Week', id: 5, name: "Friday", binary: 5, organizer_id: 5),
+                       double('Week', id: 6, name: "Saturday", binary: 6, organizer_id: 6),
+                       double('Week', id: 7, name: "Sunday", binary: 0, organizer_id: 7)]
         allow(event).to receive(:weeks).and_return(weeks_array)
 
         result = Villeme::UseCases::Dates.get_next_day_occur_human_readable(event, true)
 
-        expect(result).to eq '21/May'
+        expect(result).to eq '18/May'
       end
     end
 
