17404aea6c862205a02bd8f9b53b96c4955ef0ee
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2015-04-24 17:53:16 -0300

    Create tests to friends_from_facebook_on_villeme() and refactor

--- app/domain/usecases/friends/get_friends.rb
+++ app/domain/usecases/friends/get_friends.rb
@@ -4,31 +4,30 @@ module Villeme
       class << self
 
         def friends_from_facebook_on_villeme(entity)
-          friends = get_graph_of_friends_from_facebook_via_koala(entity)
-
-          friends.each do |fb_friend|
-            return compares_app_friends_to_facebook_friends(fb_friend, entity)
-          end
+          friends_from_facebook = get_graph_of_friends_from_facebook_via_koala(entity)
+          return compares_app_friends_to_facebook_friends(entity, friends_from_facebook)
+        rescue
+          []
         end
-
 
 
         private
 
-        def compares_app_friends_to_facebook_friends(fb_friend, entity)
+        def compares_app_friends_to_facebook_friends(entity, friends)
           list_of_friends_from_facebook ||= []
 
-          entity.city.users.each do |friend|
-            if fb_friend["name"] ==!nil and fb_friend["name"].split.first == friend.first_name
-              if entity.are_friends?(friend) == false and entity.are_friedship_invite?(friend) == false
-                list_of_friends_from_facebook << friend
+          friends.each do |fb_friend|
+            entity.city.users.each do |friend|
+              if fb_friend["name"] != nil and fb_friend["name"].split.first == friend.first_name
+                if entity.are_friends?(friend) == false and entity.are_friedship_invite?(friend) == false
+                  list_of_friends_from_facebook << friend
+                end
               end
             end
           end
 
           list_of_friends_from_facebook
         end
-
 
         def get_graph_of_friends_from_facebook_via_koala(entity)
           graph = Koala::Facebook::API.new(entity.token)
