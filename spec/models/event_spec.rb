4c4c50190f9645109bc3bf7c3a835027e653c479
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2015-07-12 00:36:49 -0300

    #DEV33: Adjust in tests

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -47,7 +47,7 @@ describe Event, type: :model do
         event = create(:event, neighborhood_name: 'Pine Hills', address: '502 Washington Avenue, Albany, NY 12203, USA', name: Faker::Lorem.sentence(2, false, 4))
       end
       create(:event, neighborhood_name: 'Partenon', address: 'Rua Rivadavia Correia, 08 - Partenon')
-      user = build(:user, neighborhood_name: 'Park South')
+      user = build(:user, neighborhood_name: 'Pine Hills')
              allow(user).to receive(:neighborhood).and_return build(:neighborhood, name: 'Pine Hills')
 
       expect(Event.all_in_my_neighborhood(user).count).to eq(3)
